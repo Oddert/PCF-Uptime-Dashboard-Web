3babc470940bf5537d1f94ea1f0bb9297468768d
Robyn F H Veitch <oddert@gmail.com>
2025-12-08 10:47:14 +0000

    change: added a typogrphy section to theme. modified the font size of h3

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,10 +2,12 @@ import { type ThemeOptions, createTheme } from '@mui/material';
 
 import breakpoints from './breakpoints';
 import palette from './palette';
+import typography from './typography';
 
 const themeOptions: ThemeOptions = {
     breakpoints,
     palette,
+    typography,
 };
 
 const theme = createTheme(themeOptions);
---
+++ src/theme/typography.ts
@@ -0,0 +1,9 @@
+import type { Theme } from '@mui/material';
+
+const typography: Partial<Theme['typography']> = {
+    h3: {
+        fontSize: '24px',
+    },
+};
+
+export default typography;
