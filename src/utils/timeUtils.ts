eb71120b9e89e8430bdac28cc7d71150faa6ad74
Robyn F H Veitch <oddert@gmail.com>
2025-12-15 13:32:02 +0000

    doc: added missing docstring

--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -3,6 +3,11 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 
 dayjs.extend(relativeTime);
 
+/**
+ * Displays a string describing time since an event in readable format, e.g. "40 seconds ago", "3 days ago", "last year".
+ * @param unixTime The timestamp in the past as a Unix formatted time code.
+ * @returns The formatted string.
+ */
 export const displayTimeFrom = (unixTime: number) => {
     const now = dayjs();
     const thePast = dayjs(unixTime);
